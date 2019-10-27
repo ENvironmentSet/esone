c9006dd68d234cb549b9398c411f020560152f7f
ENvironmentSet <sbshw1@gmail.com>
2019-10-27 15:07:22 +0900

    feat(esone): add dropLeftWhile

--- packages/esone/src/utils/index.ts
+++ packages/esone/src/utils/index.ts
@@ -1,13 +1,16 @@
 import tail from './tail';
 import init from './init';
 import dropRightWhile from './dropRightWhile';
+import dropLeftWhile from './dropLeftWhile';
 
 export * from './tail';
 export * from './init';
 export * from './dropRightWhile';
+export * from './dropLeftWhile';
 
 export default {
   tail,
   init,
-  dropRightWhile
+  dropRightWhile,
+  dropLeftWhile,
 }
