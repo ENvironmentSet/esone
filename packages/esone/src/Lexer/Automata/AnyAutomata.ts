3b06193332af1dc5ceac8c3f1c686a8f6aadc3e4
ENvironmentSet <sbshw1@gmail.com>
2020-01-24 20:16:48 +0900

    <refactor>(Acropolis/esone/Lexer/Automata): export AnyAutomata's equal function

--- packages/esone/src/Lexer/Automata/AnyAutomata.ts
+++ packages/esone/src/Lexer/Automata/AnyAutomata.ts
@@ -8,17 +8,19 @@ function anyAutomataConstructor<A extends Automata<A>>(automata: A): AnyAutomata
   return eliminator => eliminator(automata);
 }
 
+export function equals(x: AnyAutomata, y: AnyAutomata): boolean {
+  return x(innerX =>
+    y(innerY => {
+      //@FIXME: even this coercion is safe, we need natural solution
+      if (innerX instanceof innerY.constructor) return Automata.equals(innerY, unsafeCoerce(innerX));
+      else if (innerY instanceof innerX.constructor) return Automata.equals(innerX, unsafeCoerce(innerY));
+      else return false;
+    })
+  )
+}
+
 const anyAutomataInstanceDictionary: Eq<AnyAutomata> = {
-  equals(x, y): boolean {
-    return x(innerX =>
-      y(innerY => {
-        //@FIXME: even this coercion is safe, we need natural solution
-        if (innerX instanceof innerY.constructor) return Automata.equals(innerY, unsafeCoerce(innerX));
-        else if (innerY instanceof innerX.constructor) return Automata.equals(innerX, unsafeCoerce(innerY));
-        else return false;
-      })
-    )
-  }
+  equals,
 };
 
 export const anyAutomata = Object.assign(anyAutomataConstructor, anyAutomataInstanceDictionary);
