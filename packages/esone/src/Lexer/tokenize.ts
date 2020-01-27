017c0603817ba9174b6af212924cafe3c9297709
ENvironmentSet <sbshw1@gmail.com>
2020-01-28 01:05:49 +0900

    <refactor>(Acropolis/esone/Lexer): refactor tokenizer

--- packages/esone/src/Lexer/tokenize.ts
+++ packages/esone/src/Lexer/tokenize.ts
@@ -1,40 +1,59 @@
 import { pipe } from 'fp-ts/lib/pipeable';
-import { map, flatten, last, scanLeft, filterMap, filter, isEmpty, getMonoid } from 'fp-ts/lib/Array';
-import { Option, map as fmap, chain, getMonoid as getMonoidOfOption, some } from 'fp-ts/lib/Option';
+import { last, scanLeft, filterMap, isEmpty, cons, map, filter, flatten } from 'fp-ts/lib/Array';
+import { Option, map as fmap, some } from 'fp-ts/lib/Option';
 import { Automata, anyAutomata, AnyAutomata } from './Automata';
 import { StringRecognizers, automataToTokenConstructor, Token } from './LexemeRecognizers';
-import { dropRightWhile } from '../utils/Array';
-import { stringToChars } from '../utils/String';
-import { flow } from 'fp-ts/lib/function';
-import { chain as bind, of } from '../utils/Func';
+import { dropRightWhile, tail } from '../utils/Array';
+import { stringToCharList } from '../utils/String';
+import { flow, Predicate, constant } from 'fp-ts/lib/function';
+import { chain as mergeBranchedPipe, func } from '../utils/Func';
+import { getOptionM } from 'fp-ts/lib/OptionT';
 
-const { concat } = getMonoidOfOption(getMonoid<Token>());
+const { chain: mergeBranchedOptionPipe } = getOptionM(func);
 
-export const tokenize: (source: string) => Option<Token[]> = bind(
-  stringToChars,
-  chars => source => pipe(
-    chars,
-    scanLeft(StringRecognizers, (prevAutomatas: AnyAutomata[], char: string) => pipe(
+const If: <A, R>(cond: Predicate<A>, onTrue: (a: A) => R, onFalse: () => R) => (a: A) => R =
+  (cond, onTrue, onFalse) => a => cond(a) ? onTrue(a) : onFalse();
+
+const scanLeftAndExcludeInitialState: typeof scanLeft = (init, reducer) => flow(
+  scanLeft(init, reducer),
+  tail
+);
+
+//@TODO refactor getNextToken
+const getNextToken: (source: string) => Option<Token> = mergeBranchedOptionPipe(
+  flow(
+    stringToCharList,
+    scanLeftAndExcludeInitialState(StringRecognizers, (prevAutomatas: AnyAutomata[], char: string) => pipe(
       prevAutomatas,
-      filterMap(automata => automata(innerAutomata => fmap(anyAutomata)(Automata.transit(innerAutomata, char))))
+      filterMap(automata => automata(innerAutomata => fmap(anyAutomata)(Automata.transit(innerAutomata, char)))),
     )),
     map(filter(innerAutomata => innerAutomata(Automata.isOnFinalState))),
     dropRightWhile(isEmpty),
-    bind(
-      flow(
+    mergeBranchedPipe<AnyAutomata[][], number, Option<[typeof Token, number]>>(
+      arr => arr.length,
+      length => flow(
         flatten,
         last,
-      ),
-      longestMatch => bind(
-        automataMatrix => [source.slice(0, automataMatrix.length - 1), source.slice(automataMatrix.length - 1)],
-        ([usedString, unusedString]) => of(
-          chain(flow(
-            automataToTokenConstructor,
-            Token => new Token(usedString),
-            token => concat(some([token]), tokenize(unusedString))
-          ))(longestMatch),
-        )
+        fmap(automataToTokenConstructor),
+        fmap(TokenConstructor => [TokenConstructor, length] as [typeof Token, number])
       )
     )
-  )
+  ),
+  ([TokenConstructor, length]) => source => some(new TokenConstructor(source.slice(0, length)))
+);
+
+const getUnmatchedString: (matchedString: string) => (source: string) => string =
+    matchedString => source => source.slice(matchedString.length);
+
+export const tokenize: (source: string) => Option<Token[]> = If(
+  source => source.length > 0,
+  mergeBranchedOptionPipe(
+    getNextToken,
+    token => flow(
+      getUnmatchedString(token.lexeme),
+      tokenize,
+      fmap(tokens => cons(token, tokens))
+    )
+  ),
+  constant(some([] as Token[]))
 );
