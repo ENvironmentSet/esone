a6346b935224ca0fb9b6f050d24aa777faec68f6
ENvironmentSet <sbshw1@gmail.com>
2020-03-04 16:36:26 +0900

    <refactor>(Acropolis/esone/Parser): enhance typing of parse

--- packages/esone/src/Parser/parse.ts
+++ packages/esone/src/Parser/parse.ts
@@ -1,13 +1,13 @@
 import { Token } from '../Lexer';
 import { Option, fromPredicate, map, chain } from 'fp-ts/lib/Option';
 import parseProgram from './parseProgram';
-import AST from './AST';
+import { Program } from './AST';
 import { pipe } from 'fp-ts/lib/pipeable';
 import { fst } from 'fp-ts/lib/Tuple';
 import { isMeaninglessToken } from './isMeaninglessToken';
 
-export function parse(tokens: Token[]): Option<AST> {
-  const parsed: Option<[AST, Token[]]> = parseProgram(tokens);
+export function parse(tokens: Token[]): Option<Program> {
+  const parsed: Option<[Program, Token[]]> = parseProgram(tokens);
 
   return pipe(
     parsed,
