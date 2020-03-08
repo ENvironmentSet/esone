15fd423f9be8457562b6d2ff27ef8bfa1a7c7786
ENvironmentSet <sbshw1@gmail.com>
2020-03-08 17:52:15 +0900

    <fix>(Acropolis/esone/Evaluator/Internal/Type/Specification): Fix type of ES1Reference's base object

--- packages/esone/src/Evaluator/Internal/Type/Specification/ES1Reference.ts
+++ packages/esone/src/Evaluator/Internal/Type/Specification/ES1Reference.ts
@@ -1,10 +1,10 @@
 import { ES1Type } from '../ES1Type';
-import { AnyES1Object } from '../ECMAScript';
+import { AnyES1Object, AnyES1Null } from '../ECMAScript';
 import { ES1Value } from '../../ES1Value';
 
 export class ES1Reference<Representation> extends ES1Type<Representation> {
   constructor(
-    public readonly makeReference: (baseObject: AnyES1Object, propertyName: string) => Representation
+    public readonly makeReference: (baseObject: AnyES1Object | AnyES1Null , propertyName: string) => Representation
   ) {
     super();
   }
@@ -16,7 +16,7 @@ export function anyES1Reference<R, T extends ES1Reference<R>>(x: ES1Value<R, T>)
 }
 
 type NaiveES1ListRepresentationCalculus<Representation> = [
-  (baseObject: AnyES1Object, propertyName: string) => Representation
+  (baseObject: AnyES1Object | AnyES1Null, propertyName: string) => Representation
 ]
 
 class NaiveES1Reference<Representation> extends ES1Reference<Representation> {
@@ -26,7 +26,7 @@ class NaiveES1Reference<Representation> extends ES1Reference<Representation> {
 }
 
 class NaiveES1ReferenceRepresentation {
-  constructor(public readonly baseObject: AnyES1Object, public readonly propertyName: string) {}
+  constructor(public readonly baseObject: AnyES1Object | AnyES1Null, public readonly propertyName: string) {}
 }
 
 export const es1Reference: ES1Reference<NaiveES1ReferenceRepresentation> = new NaiveES1Reference(
