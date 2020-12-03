f036bae111b7a1f0e5f35ef58220d3ddc1509001
ENvironmentSet <sbshw1@gmail.com>
2020-12-03 17:50:39 +0900

    <deps>(Acropolis/esone/): Add Layered

--- packages/esone/src/utils/Layered/Layered.ts
+++ packages/esone/src/utils/Layered/Layered.ts
@@ -1,20 +1,9 @@
-import { Option, some, chain } from 'fp-ts/Option';
-
-export const previous = Symbol('Layered/previous');
-
 export class Layered {
-  readonly [previous]: Option<this>;
-
-  next(update: (self: this) => void): this { //@FIXME: Better ways?
+  next(update: (self: this) => void): this {
     const isolated = Object.create(this);
 
     update(isolated);
 
-    return Object.assign(isolated, { [previous]: some(this) });
-  }
-
-  slice(ordinal: number): Option<this> {
-    if (ordinal === 0) return some(this);
-    else return chain((previous: this) => previous.slice(ordinal - 1))(this[previous]);
+    return Object.assign(isolated, update);
   }
 }
