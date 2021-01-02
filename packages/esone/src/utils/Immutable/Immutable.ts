9a327a350a79be807adc085a95566b75055d0d75
ENvironmentSet <sbshw1@gmail.com>
2021-01-03 04:52:24 +0900

    <refactor>(Acropolis/esone/): use unknown

--- packages/esone/src/utils/Immutable/Immutable.ts
+++ packages/esone/src/utils/Immutable/Immutable.ts
@@ -1,6 +1,6 @@
 export class Immutable {
   //@FIXME: Since typescript implements variance incorrectly, other type-safe ways are as unsafe as this.
-  update(update: any): this {
+  update(update: unknown): this {
     const isolated = Object.create(this);
 
     return Object.assign(isolated, update);
