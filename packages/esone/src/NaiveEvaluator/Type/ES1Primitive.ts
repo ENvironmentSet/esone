dd7e9e8547cf20d6ad1cbce6bfd5186b62452b19
ENvironmentSet <sbshw1@gmail.com>
2021-01-06 16:58:53 +0900

    Add ES1Primitive

---
+++ packages/esone/src/NaiveEvaluator/Type/ES1Primitive.ts
@@ -0,0 +1,7 @@
+import { ES1Boolean } from './ES1Boolean';
+import { ES1Number } from './ES1Number';
+import { ES1String } from './ES1String';
+import { ES1Null } from './ES1Null';
+import { ES1Undefined } from './ES1Undefined';
+
+export type ES1Primitive = ES1Boolean | ES1Number | ES1String | ES1Null | ES1Undefined;
