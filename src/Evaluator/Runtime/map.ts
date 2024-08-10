5cc3e81c9e07c879ffedc5561a1a103254124213
ENvironmentSet <sbshw1@gmail.com>
2024-08-10 23:50:11 +0900

    fix import

--- src/Evaluator/Runtime/map.ts
+++ src/Evaluator/Runtime/map.ts
@@ -1,5 +1,5 @@
 import { lift, Runtime } from './Runtime';
-import { map as optionMap } from 'fp-ts/es6/Option';
+import { map as optionMap } from 'fp-ts/Option';
 
 export function map<A, B>(f: (value: A) => B): (runtime: Runtime<A>) => Runtime<B> {
   return lift(optionMap(f));
