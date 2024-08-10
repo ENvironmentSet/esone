583bf8ee53d7316ba073e3a08d523b21ea20004d
ENvironmentSet <sbshw1@gmail.com>
2024-08-10 23:31:58 +0900

    make global scope reference 'real' global scope host defined

--- src/Evaluator/compile.ts
+++ src/Evaluator/compile.ts
@@ -6,6 +6,7 @@ import { compileDeclaration } from './Declaration/compileDeclaration';
 import { compileStatement } from './Statement/compileStatement';
 import { ES1Value } from './Type/ES1Value';
 import { none, Option } from 'fp-ts/Option';
+import { some } from 'fp-ts/lib/Option';
 
 const compileSourceElement: (ast: AST, escape: (result: Option<ES1Value>) => Runtime<ES1Value>) => Runtime<ES1Value>
   = (ast, escape) => match(
@@ -15,4 +16,4 @@ const compileSourceElement: (ast: AST, escape: (result: Option<ES1Value>) => Run
   )(ast);
 
 export const compile: (program: Program) => Runtime<ES1Value>
-  = program => isolate(none, escape => compose(program.sourceElements.reduce((prev, sourceElement) => compose(prev, compileSourceElement(sourceElement, escape)), empty<ES1Value>()), escape(none)));
+  = program => context => isolate<ES1Value>(some(context.currentScope), escape => compose(program.sourceElements.reduce((prev, sourceElement) => compose(prev, compileSourceElement(sourceElement, escape)), empty<ES1Value>()), escape(none)))(context);
