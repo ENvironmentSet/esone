1eff331d741fe02f5034aa72789a4136d2055eee
ENvironmentSet <sbshw1@gmail.com>
2024-08-10 23:43:04 +0900

    refactor if statement compiler

--- src/Evaluator/Statement/compileIf.ts
+++ src/Evaluator/Statement/compileIf.ts
@@ -1,24 +1,21 @@
 import { If } from '../../Parser/AST/Statement/If';
-import { isNone, Option } from 'fp-ts/Option';
+import { Option } from 'fp-ts/Option';
 import { ES1Value } from '../Type/ES1Value';
-import { Runtime, extend, error, empty } from '../Runtime/Runtime';
+import { Runtime, empty } from '../Runtime/Runtime';
 import { compileExpression } from '../Expression/compileExpression';
 import { ES1Boolean } from '../Type/ES1Boolean';
 import { compileStatement } from './compileStatement';
+import { extendWithValue } from '../Runtime/extendWithValue';
 
 export const compileIf: (ifStatement: If, escape: (result: Option<ES1Value>) => Runtime<ES1Value>) => Runtime<ES1Value>
-  = (ifStatement, escape) => extend(
+  = (ifStatement, escape) => extendWithValue(
     compileExpression(ifStatement.condition),
-    cond => isNone(cond)
-      ? error('condition was not evaluated to value')
-      : extend(
-        cond.value.toBoolean(),
-        cond => isNone(cond)
-          ? error('condition was not evaluated to boolean')
-          : ES1Value.equals(cond.value, ES1Boolean.ES1True())
-            ? compileStatement(ifStatement.then, escape)
-            : ifStatement.elseClause
-              ? compileStatement(ifStatement.elseClause, escape)
-              : empty()
-      )
+    cond => extendWithValue(
+      cond.toBoolean(),
+      cond => ES1Value.equals(cond, ES1Boolean.ES1True())
+        ? compileStatement(ifStatement.then, escape)
+        : ifStatement.elseClause
+          ? compileStatement(ifStatement.elseClause, escape)
+          : empty()
+    )
 );
